253c80cd5ef8b6999349dc326a89aded77a4a1d9
philippark <philippark271@gmail.com>
2024-07-10 13:02:03 -0400

    takes in command line args

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-int main(){}
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv){
+    if (argc != 6){
+        std::cout << "Not enough arguments" << std::endl;
+        return -1;
+    }
+
+    int n = std::stoi(*(argv+1));
+    int n_cpu = std::stoi(*(argv+2));
+    int seed = std::stoi(*(argv+3));
+    int lambda = std::stoi(*(argv+4));
+    int random_upper_bound = std::stoi(*(argv+5));
+
+}
