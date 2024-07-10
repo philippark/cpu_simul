377d574fd09d3fea8bffc554377a88f78af9d027
philippark <philippark271@gmail.com>
2024-07-10 14:49:38 -0400

    shaping the code structure, next_expn is blackbox

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+# include <cstdlib>
+# include <iostream>
+# include <iomanip>
+# include <ctime>
+
+double next_expn(double seed, double lambda){
+    return 1;
+}
 
 int main(int argc, char** argv){
     if (argc != 6){
@@ -12,5 +23,34 @@ int main(int argc, char** argv){
     int seed = std::stoi(*(argv+3));
     int lambda = std::stoi(*(argv+4));
     int random_upper_bound = std::stoi(*(argv+5));
+
+    
+
+    for (int i = 0; i < n; i++){
+        int arrival_time = floor(next_expn(seed, lambda));
+
+        int num_cpu_bursts = ceiling(drand() * 32);
+
+        for (int j = 0; j < num_cpu_bursts; j++){
+            int cpu_burst_time = next_exp(seed, lambda);
+
+            //if last cpu burst, dont generate anymore
+            if (j == num_cpu_bursts-1) continue;
+
+
+            int io_burst_time = next_exp(seed, lambda);
+
+            while (io_burst_time <= 2*cpu_burst_time){
+                io_burst_time *= 8;
+            }
+
+            //if cpu bound
+            if (i < n_cpu){
+                cpu_burst_time *- 4;
+                io_burst_time /= 8;
+            }
+
+        }
+    }
 
 }
